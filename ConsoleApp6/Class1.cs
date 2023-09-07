21ff4e4efdf8f891270b46236862b45f5faff9c8
tanyakarpova1 <113306185+tanyakarpova1@users.noreply.github.com>
2023-09-07 21:09:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Class1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ApplicationContext;
+using static ConsoleApp6.ClassroomsEntity;
+using static ConsoleApp6.ExamEntity;
+
+namespace ConsoleApp6
+{
+    class StudentEntity
+    {
+        public class Student
+        {
+            public Student()
+            {
+                exam = new List<Exam>();
+            }
+            [Key]
+            public int studentid { get; set; }
+            public string name { get; set; }
+            public int classroom_id { get; set; }
+            public Classrooms classroom_ { get; set; }
+            public virtual List<Exam> exam { get; set; }
+            public override string ToString()
+            {
+                return $"Student: name = {name}, classrom_id = {classroom_id}";
+            }
+        }
+    }
+}
---
+++ ConsoleApp6/Class2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ApplicationContext;
+using static ConsoleApp6.StudentEntity;
+
+namespace ConsoleApp6
+{
+
+    class ClassroomsEntity
+    {
+        public class Classrooms
+        {
+            public Classrooms()
+            {
+                students = new List<Student>();
+            }
+            public int id { get; set; }
+            public string teacher { get; set; }
+
+            public virtual List<Student> students { get; set; }
+            public override string ToString()
+            {
+                return $"classroom: teacher = {teacher}";
+            }
+        }
+    }
+}
